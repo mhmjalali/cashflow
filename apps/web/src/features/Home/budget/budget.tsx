83c0cd180e73e96515a512626f01fdaee472ab68
mhmjalali <muhammad.art79@gmail.com>
2026-06-28 17:08:40 +0330

    working on home_page

--- apps/web/src/features/Home/budget/budget.tsx
+++ apps/web/src/features/Home/budget/budget.tsx
@@ -1,10 +1,8 @@
-import BudgetOverview from "./budget-overview";
 import BudgetList from "./budget-list";
 
 const Budget = () => {
   return (
     <div className="flex flex-col gap-4">
-      <BudgetOverview />
       <BudgetList />
     </div>
   );
--- apps/web/src/features/Home/index.tsx
+++ apps/web/src/features/Home/index.tsx
@@ -1,10 +1,12 @@
 import Budget from "./budget/budget";
-import FinantialSnapShot from "./FinantialSnapShot";
+import FinantialSnapShot from "./finantial-snap-shot";
+import QuickActions from "./quick-actions";
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-4">
       <FinantialSnapShot />
+      <QuickActions />
       <Budget />
     </div>
   );
