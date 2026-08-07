526c3fc9276c85f9250670f5a2bdd61b8ccc89ee
unknown <muhammad.art79@gmail.com>
2026-08-07 13:03:01 -0700

    complete adding structure for eslint prettier and tsconfig files error and other things that need to be set done

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { nextJsConfig } from "@cashflow/eslint-config/next-js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...nextJsConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
